ab0ccfcfc0b66a45bd79ae2a3df4190a080e57d5
duda090 <eduardasousas005@gmail.com>
2024-09-24 21:15:44 -0300

    eduarda
    abrindo uma imagem com open cv

--- eduarda.py
+++ eduarda.py
@@ -1,1 +1,18 @@
-print("hello")
+# Importa a biblioteca OpenCV para manipulação de imagens e vídeos
+import cv2
+
+# Importa a biblioteca NumPy, usada para manipulação de arrays numéricos
+import numpy as np
+
+# Carrega a imagem 'eduarda.jpg' em cores (RGB)
+# O argumento 'cv2.IMREAD_COLOR' indica que a imagem será carregada com cores
+img = cv2.imread('eduarda.jpg', cv2.IMREAD_COLOR)
+
+# Cria uma janela chamada 'hello', onde a imagem será exibida
+cv2.namedWindow('hello')
+
+# Exibe a imagem carregada na janela 'hello'
+cv2.imshow('hello', img)
+
+# Aguarda até que uma tecla seja pressionada antes de fechar a janela
+cv2.waitKey()
